88ffae5553c236bff29b044c79211d6fb2c43e08
larabiislem <islamlarabi0@gmail.com>
2025-12-02 12:52:41 +0100

    Add main.c file with initial setup for the project

---
+++ TP2/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "algos.h"  // Remplace par le nom de ton .h
+
+int main() {
+    // for compilation : gcc main.c algos.c -o tp2
+    // ./tp2
+
+    return 0;
+}
